6b493d3a8b6046207a9aabfdc5da376fdf33a242
Bhaskar Pandey1 <bhaskarpandey787@gmail.com>
2025-06-23 16:55:47 +0530

    Done

--- engine.py
+++ engine.py
@@ -2,3 +2,5 @@ from hangmanAI import HangmanSolver1, simulate_hangman_game
 
 solver1 = HangmanSolver1(model_path= "best_model1.pth")
 solver2 = HangmanSolver1(model_path= "best_model2.pth")
+
+result = simulate_hangman_game(solver1, solver2, "bhaskar", verbose=True)
